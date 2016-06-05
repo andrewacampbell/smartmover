f1b5685c37bc1201816baf01f7828579a2ad3048
Andrew <legero_cam@yahoo.com>
2016-06-05 00:05:43 -0500

    add new york times article through api for location selected

--- js/script.js
+++ js/script.js
@@ -15,6 +15,43 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    var streetLoc = $("#street").val();
+    var cityLoc = $("#city").val();
+    var address = streetLoc + ' ,' + cityLoc;
+
+    $greeting.text('So you want to move and live at ' + address + '?');
+    var streetViewLocator = 'https://maps.googleapis.com/maps/api/streetview?size=1280x720&location='+
+    address + '';
+
+    $body.append('<img class="bgimg" src=" '+ streetViewLocator + '">');
+
+// NY times article
+var newyorktimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityLoc +' &sort=newest&api-key=dce09e3080214712a8fed3a5360c148b';
+
+// This function takes a newyour times url and return articles for a given city
+$.getJSON(newyorktimesURL, function( data ) {
+
+
+  $("#nytimes-header").text('New York Times Articles for ' + cityLoc);
+
+  var articles = data.response.docs;
+
+ //Loop throught articles return from NY times API
+  for(var i =0; i < articles.length; i++){
+
+    var article = articles[i];
+
+    $("#nytimes-articles").append('<li class="article">'
+        + '<a href="'+article.web_url+'">'+
+        article.headline.main +
+        '</a>' + article.snippet + '</li>');
+  }).error(function (e)){
+    $("#nytimes-header").text('New York Times Articles for ' + cityLoc + could not be loaded);
+
+  }
+});
+
+
     return false;
 };
 
