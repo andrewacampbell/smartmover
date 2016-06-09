400d54eb01daef0ba64eae9afd3764256736a50b
Andrew <legero_cam@yahoo.com>
2016-06-09 06:00:23 -0500

    fix error with NYT API and added error handling, stared to implement wikipedia API

--- js/script.js
+++ js/script.js
@@ -25,30 +25,57 @@ function loadData() {
 
     $body.append('<img class="bgimg" src=" '+ streetViewLocator + '">');
 
-// NY times article
+// NY times article custom URL
 var newyorktimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityLoc +' &sort=newest&api-key=dce09e3080214712a8fed3a5360c148b';
 
-// This function takes a newyour times url and return articles for a given city
+/**
+ * [getJSON // This function takes a New York times url and return
+ *  articles for a given city these are return with a link to
+ *  the article.
+ * @param  {[type]} newyorktimesURL [custom url]
+ * @param  {[type]} function(       data
+ * @return {[type]}                 article that match the criteria for the city and street
+ */
 $.getJSON(newyorktimesURL, function( data ) {
 
+    $("#nytimes-header").text('New York Times Articles for ' + cityLoc);
 
-  $("#nytimes-header").text('New York Times Articles for ' + cityLoc);
+    var articles = data.response.docs;
 
-  var articles = data.response.docs;
+   //Loop throught articles return from NY times API
+    for(var i =0; i < articles.length; i++){
 
- //Loop throught articles return from NY times API
-  for(var i =0; i < articles.length; i++){
+        var article = articles[i];
 
-    var article = articles[i];
+        $("#nytimes-articles").append('<li class="article">'
+            + '<a href="'+article.web_url+'">'+
+            article.headline.main +
+            '</a>' + article.snippet + '</li>');
+    }
 
-    $("#nytimes-articles").append('<li class="article">'
-        + '<a href="'+article.web_url+'">'+
-        article.headline.main +
-        '</a>' + article.snippet + '</li>');
-  }).error(function (e)){
-    $("#nytimes-header").text('New York Times Articles for ' + cityLoc + could not be loaded);
+}).fail(function(){
+    $("#nytimes-header").text('No New York Times Articles for ' + cityLoc +" "+ streetLoc);
+});
 
+//https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
+// wikipedia custom URL
+var wikipediaUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch?search'+cityLoc+'&format=json&callback=wikiCallback';
+
+
+$.ajax({
+  url: wikipediaUrl,
+  datatype: "jsonp",
+  //jsonp: "callback",
+  success: function(response){
+    var wikiArtiles = response[1];
+
+    for(var i=0; i < wikiArtiles.length; i++){
+      articleList = wikiArtiles[i];
+      var url = 'http://en.wikipedia.org/wiki/'+ articleList;
+      $("#wikipedia-links").append('<li><a href"' + url + '">'+articleList + '</a></li>');
+    }
   }
+
 });
 
 
